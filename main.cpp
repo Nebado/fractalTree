d777e578668b692201fbc147d82df2fe94e6678e
Nebado <ruslan.nbd@gmail.com>
2020-11-11 02:01:33 +0200

    Initialize window, set size window.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include <time.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("Hello, World!");
+    sf::RenderWindow window(sf::VideoMode(700, 700), "Fractal Tree");
+    window.setFramerateLimit(30);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+        window.clear();
+
+        window.display();
+        usleep(1000000);
+    }
+
     return 0;
 }
